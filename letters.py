0f397d3d1bd8c9127f011bc34f2ea7fbdbaad6df
Nile Walker <niwal7@morgan.edu>
2017-11-15 11:22:11 -0500

    Now allows for character changing and is called as a function

--- letters.py
+++ letters.py
@@ -6,31 +6,34 @@ from argparse import ArgumentParser
 parser = ArgumentParser()
 parser.add_argument("-n","--name", type=str,help="The name that is greeted")
 args = parser.parse_args()
-name=(args.name.upper() if args.name != None else input("Enter a Name \n").upper())
-def make_img():
-    y, x = [int(a) for a in os.popen('stty size', 'r').read().split()]
-    size=cv2.getTextSize(name, cv2.FONT_HERSHEY_SIMPLEX, 1, thickness=2)[0]
-    img = np.zeros((y,size[0],1), np.uint8)
-    cv2.putText(img,name,(0,int(y-2)), cv2.FONT_HERSHEY_SIMPLEX, 1, (2), 2, cv2.LINE_AA)
-    frame=cv2.threshold(img, 1, 1, cv2.THRESH_BINARY)[1]
-    frame=["".join([str(i) for i in frame[a]]) for a in range(len(frame))]
-    return x,y,len(frame[0]),frame
-x,y,siz,frame=make_img()
-c=0
-while True:
-    if c==siz+x:
-        c=0
-    cy, cx = [int(a) for a in os.popen('stty size', 'r').read().split()]
-    if cx!=x:
+_name=(args.name.upper() if args.name != None else input("Enter a Name \n").upper())
+def announce(name,char=("1","0")): #
+    def make_img():
         y, x = [int(a) for a in os.popen('stty size', 'r').read().split()]
-    if cy!=y:
-        x,y,siz,frame=make_img()
-    # if cx!=x or cy!=y:
-    #    break
-    
-    os.system('clear')
-    #sys.stdout.flush()
-    print(*[i.rjust(cx+siz,"0")[c:c+cx].ljust(cx,"0") for i in frame],sep="\n")
-    c+=1
-    time.sleep(.01) 
-    #(i[:(c+cx)%cx]if (c+cx)>siz else "")
+        size=cv2.getTextSize(name, cv2.FONT_HERSHEY_SIMPLEX, 1, thickness=2)[0]
+        img = np.zeros((y,size[0],1), np.uint8)
+        cv2.putText(img,name,(0,int(y-2)), cv2.FONT_HERSHEY_SIMPLEX, 1, (2), 2, cv2.LINE_AA)
+        frame=cv2.threshold(img, 1, 1, cv2.THRESH_BINARY)[1]
+        frame=["".join([str(i) for i in frame[a]]) for a in range(len(frame))]
+        if char!=("1","0"):
+            frame=["".join(list(map(lambda x: char[0] if x=="1" else char[1],i))) for i in frame]
+        return x,y,len(frame[0]),frame
+    x,y,siz,frame=make_img()
+    c=0
+    while True:
+        if c==siz+x:
+            c=0
+        cy, cx = [int(a) for a in os.popen('stty size', 'r').read().split()]
+        if cx!=x:
+            y, x = [int(a) for a in os.popen('stty size', 'r').read().split()]
+        if cy!=y:
+            x,y,siz,frame=make_img()
+        # if cx!=x or cy!=y:
+        #    break
+        os.system('clear')
+        #sys.stdout.flush()
+        print(*[i.rjust(cx+siz,char[1])[c:c+cx].ljust(cx,char[1]) for i in frame],sep="\n")
+        c+=1
+        time.sleep(.01) 
+        #(i[:(c+cx)%cx]if (c+cx)>siz else "")
+announce(_name)
