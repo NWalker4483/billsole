17cfb7dbab5d6f29c8052c88fe87137d5b43200c
Nile Walker <niwal7@morgan.edu>
2017-11-15 11:29:02 -0500

    Changed Script to only change characters if char is not default

--- letters.py
+++ letters.py
@@ -14,16 +14,18 @@ def announce(name,char=("1","0")): #
         img = np.zeros((y,size[0],1), np.uint8)
         cv2.putText(img,name,(0,int(y-2)), cv2.FONT_HERSHEY_SIMPLEX, 1, (2), 2, cv2.LINE_AA)
         frame=cv2.threshold(img, 1, 1, cv2.THRESH_BINARY)[1]
-        frame=["".join([str(i) for i in frame[a]]) for a in range(len(frame))]
-        if char!=("1","0"):
-            frame=["".join(list(map(lambda x: char[0] if x=="1" else char[1],i))) for i in frame]
+        if char==("1","0"):
+            frame=["".join([str(i) for i in frame[a]]) for a in range(len(frame))]
+        else:
+            frame=["".join(list(map(lambda x: char[0] if x==1 else char[1],i))) for i in frame]
         return x,y,len(frame[0]),frame
     x,y,siz,frame=make_img()
     c=0
     while True:
+        cy, cx = [int(a) for a in os.popen('stty size', 'r').read().split()]
+        # Check current console size so that it can be compared to the 
         if c==siz+x:
             c=0
-        cy, cx = [int(a) for a in os.popen('stty size', 'r').read().split()]
         if cx!=x:
             y, x = [int(a) for a in os.popen('stty size', 'r').read().split()]
         if cy!=y:
